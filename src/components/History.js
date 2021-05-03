3ecb735d7375053bc1798202d12ce43581a8fd6d
Gavin Young <younnit@gmail.com>
2021-05-03 13:57:35 -0600

    got history and payments to display properly

--- src/components/History.js
+++ src/components/History.js
@@ -21,8 +21,6 @@ export class History extends Component {
                         
                     )
                 })}
-
-                <p>History test</p>
             </div>
         )
     }
